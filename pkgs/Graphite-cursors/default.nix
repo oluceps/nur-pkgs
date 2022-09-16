0a4fce40474c5bb322fdea3e3a9ebaa31f955246
oluceps <i@oluceps.uk>
2022-09-16 14:09:31 +0800

    ref: code format

--- pkgs/Graphite-cursors/default.nix
+++ pkgs/Graphite-cursors/default.nix
@@ -1,4 +1,9 @@
-{ lib, stdenv, fetchFromGitHub, inkscape, xcursorgen }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, inkscape
+, xcursorgen
+}:
 
 stdenv.mkDerivation rec {
   pname = "Graphite-cursors";
@@ -11,13 +16,13 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-Kopl2NweYrq9rhw+0EUMhY/pfGo4g387927TZAhI5/A=";
   };
 
-#  nativeBuildInputs = [ inkscape xcursorgen ];
+  #  nativeBuildInputs = [ inkscape xcursorgen ];
 
- # buildPhase = ''
-#    patchShebangs .
-#    HOME=$TMP ./build.sh
-#  '';
-#
+  # buildPhase = ''
+  #    patchShebangs .
+  #    HOME=$TMP ./build.sh
+  #  '';
+  #
   installPhase = ''
     install -dm 755 $out/share/icons
     # cp -dr --no-preserve='ownership' dist{-dark{,-nord},-light{,-nord}} $out/share/icons/
@@ -25,13 +30,13 @@ stdenv.mkDerivation rec {
     mv dist-light $out/share/icons/Graphite-light
     mv dist-dark-nord $out/share/icons/Graphite-dark-nord
     mv dist-light-nord $out/share/icons/Graphite-light-nord
-    '';
+  '';
 
   meta = with lib; {
     description = "Graphite cursor theme";
     homepage = "https://github.com/vinceliuice/Graphite-cursors";
     license = licenses.gpl3;
     platforms = platforms.all;
-#    maintainers = with maintainers; [ oluceps ];
+    #    maintainers = with maintainers; [ oluceps ];
   };
 }
